8af9e6a847b2dfd62aaa2ba017e2678145b370ef
YoussefNKH <youssef1nakhli@gmail.com>
2025-12-21 11:31:05 +0100

    correction

--- backend/app/models/batch_prediction_output.py
+++ backend/app/models/batch_prediction_output.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import List
 
 class BatchPredictionOutput(BaseModel):
+    model_config = ConfigDict(protected_namespaces=())
     predictions: List[float]
     model_name: str
     model_version: str
